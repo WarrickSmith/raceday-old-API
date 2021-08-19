89e9e919f35228b641fb5fce9fd646516edccbad
Warrick <warricksmith@developersinstitute.co.nz>
2021-08-19 14:11:50 +1200

    feat: add basic html to sjhow racedays.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import racedays from './Data/racedays';
+import PageHeader from './components/PageHeader/PageHeader';
+import getRacing from './Services/getRaceing/getRacing';
+import ShowRaceDays from './components/ShowRaceDays/ShowraceDays';
 
 function App() {
   return (
     <div className='container'>
-      <h1>RaceDay</h1>
+      <PageHeader title={'RaceDay'} />
+      <ShowRaceDays racedays={racedays} />
     </div>
   );
 }
---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,11 @@
+import './PageHeader.css';
+
+const PageHeader = ({ title }) => {
+  return (
+    <div className='page-header'>
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+export default PageHeader;
