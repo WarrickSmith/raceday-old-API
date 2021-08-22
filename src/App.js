8c629c742501fbf4b66e0cdf687131ff763f16bf
Warrick <warricksmith@developersinstitute.co.nz>
2021-08-22 14:41:20 +1200

    fix: remove unncecssary import

--- src/App.js
+++ src/App.js
@@ -11,24 +11,36 @@ function App() {
   // define application hooks
   const [raceMeetings, setRaceMeetings] = useState(null);
   const [races, setRaces] = useState(null);
-  const [currentMeeting, setCurrentMeeting] = useState(0);
+  const [raceMeeting, setRaceMeeting] = useState(0);
 
   const loadData = async () => {
     const newRaceMeetings = await getRacing();
     setRaceMeetings(newRaceMeetings);
     console.log(`newRaceMeetings: `, newRaceMeetings);
-    setRaces(newRaceMeetings.Meetings[currentMeeting].Races);
-    console.log(`Races: `, newRaceMeetings.Meetings[currentMeeting].Races);
+    setRaces(newRaceMeetings.Meetings[raceMeeting].Races);
+    console.log(`Races: `, newRaceMeetings.Meetings[raceMeeting].Races);
   };
 
   useEffect(() => {
     loadData();
   }, []);
 
+  useEffect(() => {
+    if (raceMeetings === null) {
+      console.log(`No race Meetings in raceMeetins variable`);
+    } else {
+      console.log(`Updated Races: `, raceMeetings);
+      setRaces(raceMeetings.Meetings[raceMeeting].Races);
+    }
+  }, [raceMeeting]);
+
   return (
     <div className='container'>
       <PageHeader title={'RaceDay'} />
-      <ShowRaceMeetings raceMeetings={raceMeetings} />
+      <ShowRaceMeetings
+        raceMeetings={raceMeetings}
+        setRaceMeeting={setRaceMeeting}
+      />
       <ShowRaces races={races} />
       <button onClick={loadData}>Get Race data</button>
     </div>
