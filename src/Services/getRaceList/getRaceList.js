7444918236c8a3b372c7cc41410a35faa216a219
Warrick <warricksmith@developersinstitute.co.nz>
2021-08-25 19:28:31 +1200

    fix: add ate field to raceList objects

--- src/Services/getRaceList/getRaceList.js
+++ src/Services/getRaceList/getRaceList.js
@@ -1,6 +1,6 @@
 const getRaceList = (raceMeetings) => {
   console.log(`getRaceList Function Executing....`, raceMeetings);
-  const raceList = [];
+  let newRaceList = [];
   for (const meeting of raceMeetings) {
     const races = meeting.Races;
     for (const race of races) {
@@ -10,12 +10,16 @@ const getRaceList = (raceMeetings) => {
       newObject.VenueName = meeting.VenueName;
       newObject.MeetingType = meeting.MeetingType;
       newObject.RaceNumber = race.RaceNumber;
-      newObject.RaceTime = race.RaceTime;
-      raceList.push(newObject);
+      newObject.RaceTime = new Date(race.RaceTime);
+      newRaceList.push(newObject);
     }
   }
-
-  console.log(`The RaceList is: `, raceList);
+  // Sort racelist ascending by RaceTime
+  const raceList = newRaceList.sort((a, b) => {
+    const c = new Date(a.RaceTime);
+    const d = new Date(b.RaceTime);
+    return c - d;
+  });
   return raceList;
 };
 
