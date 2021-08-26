33255d4e591751772d6389c844550d646d518e2b
Warrick <warricksmith@developersinstitute.co.nz>
2021-08-27 10:22:50 +1200

    fix: add comnt & error handling to getracemeetings

--- src/Services/getRaceMeetings/getRaceMeetings.js
+++ src/Services/getRaceMeetings/getRaceMeetings.js
@@ -1,10 +1,21 @@
+// Function to fetch a racedays full meeting, race and runner information including win/place odds and racepools.
 const getRaceMeetings = async (raceString) => {
-  const response = await fetch(
-    `https://api.tatts.com/svc/sales/vmax/web/data/racing${raceString}`
-  );
-  const newRaceday = await response.json();
-  console.log(`RaceDay Data Fetched: `, newRaceday);
-  return newRaceday;
+  try {
+    const response = await fetch(
+      `https://api.tatts.com/svc/sales/vmax/web/data/racing${raceString}`
+    );
+    const newRaceday = await response.json();
+    console.log(`RaceDay Data Fetched: `, newRaceday);
+    return newRaceday;
+  } catch (error) {
+    alert(
+      `There has been a problem fetching Race Meetings (getRaceMeetings):
+      
+Please re-load the web page.`,
+      error
+    );
+    return;
+  }
 };
 
 export default getRaceMeetings;
