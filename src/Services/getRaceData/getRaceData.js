01d59139a7326d061df8b6ec50e322c7b2d2de87
Warrick <warricksmith@developersinstitute.co.nz>
2021-08-27 10:32:26 +1200

    fix: add cmnt and error handling for getRaceData

--- src/Services/getRaceData/getRaceData.js
+++ src/Services/getRaceData/getRaceData.js
@@ -1,18 +1,30 @@
 const getRaceData = async (raceString, newRaceMeetings) => {
-  // Iterate meeting and determine meeting code, assign code to string
-  let raceData = [];
-  for (const meeting of newRaceMeetings.RaceDay.Meetings) {
-    // Fetch meeting using code, push result to newRaceData array
+  // Function to fetch a full meeting including, races and runner information including win/place odds and racepools. This is pushed to an array (raceData) and returned. This in turned is used by another function (getRaceList) to build a list of races for the RaceSelector component.
+  const raceData = [];
 
-    let meetingCode = meeting.MeetingCode;
-    const response = await fetch(
-      `https://api.tatts.com/svc/sales/vmax/web/data/racing${raceString}/${meetingCode}`
+  // Iterate meeting and determine meeting code, assign code to string for fetching a specific meeting.
+  try {
+    for (const meeting of newRaceMeetings.RaceDay.Meetings) {
+      // Fetch meeting using code, push result to newRaceData array
+
+      const meetingCode = meeting.MeetingCode;
+      const response = await fetch(
+        `https://api.tatts.com/svc/sales/vmax/web/data/racing${raceString}/${meetingCode}`
+      );
+      const newRacedata = await response.json();
+      raceData.push(newRacedata.RaceDay.Meetings[0]);
+    }
+    console.log(`raceData Fetched`, raceData);
+    return raceData;
+  } catch (error) {
+    alert(
+      `There has been a problem fetching detailed Race Meeting data (getRaceData):
+    
+Please re-load the web page.`,
+      error
     );
-    let newRacedata = await response.json();
-    raceData.push(newRacedata.RaceDay.Meetings[0]);
+    return;
   }
-  console.log(`raceData Fetched`, raceData);
-  return raceData;
 };
 
 export default getRaceData;
