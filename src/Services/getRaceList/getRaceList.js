03dfdb58bef090e8a29f23b8c2ea8c8d5b336ce3
Warrick <warricksmith@developersinstitute.co.nz>
2021-08-26 16:55:18 +1200

    fix: add functionconverting au to nz time

--- src/Services/getRaceList/getRaceList.js
+++ src/Services/getRaceList/getRaceList.js
@@ -1,4 +1,16 @@
 const getRaceList = (raceMeetings) => {
+  /* This function takes the raceMeetings array and parses it for each race in a race meeting to produce an array of races (raceList). This is then sorted in acending order by race start time(RaceTime) and the raceList is returned for use with the RaceSelector component */
+
+  // Function to read Australian date/time and convert to NZ time for storing in raceList array. hard coded to 2hrs difference. Will need to pdtae to use a better date/time conversion library.
+
+  const nswTime = (time) => {
+    let utc_Offset = time.getTimezoneOffset();
+    time.setMinutes(time.getMinutes() + utc_Offset);
+    let nsw_Offset = 14 * 60;
+    time.setMinutes(time.getMinutes() + nsw_Offset);
+    return time;
+  };
+
   console.log(`getRaceList Function Executing....`, raceMeetings);
   let newRaceList = [];
   for (const meeting of raceMeetings) {
@@ -10,7 +22,7 @@ const getRaceList = (raceMeetings) => {
       newObject.VenueName = meeting.VenueName;
       newObject.MeetingType = meeting.MeetingType;
       newObject.RaceNumber = race.RaceNumber;
-      newObject.RaceTime = new Date(race.RaceTime);
+      newObject.RaceTime = nswTime(new Date(race.RaceTime));
       newRaceList.push(newObject);
     }
   }
