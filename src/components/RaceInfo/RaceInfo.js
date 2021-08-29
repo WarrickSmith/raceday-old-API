20ec793b5f7364cd13855d40904ec8574977f069
Warrick <warricksmith@developersinstitute.co.nz>
2021-08-30 09:43:32 +1200

    fix: change logic for detecting no races

--- src/components/RaceInfo/RaceInfo.js
+++ src/components/RaceInfo/RaceInfo.js
@@ -2,26 +2,26 @@
 const RaceInfo = ({ raceList, currentRace }) => {
   console.log(`RaceInfo Element Loading...`);
   console.log(`currentRace: `, currentRace);
-  if (raceList == null || !currentRace) {
+  if (raceList == null || currentRace === undefined) {
     return (
       <div>
         <h2>Loading Race Info</h2>
       </div>
     );
-  } else
-    return (
-      <div className='show-raceinfo'>
-        <h2>{raceList[currentRace].RaceName}</h2>
-        <h3>RACE DISTANCE - {raceList[currentRace].RaceDistance}</h3>
-        <h3>RUNNERS - {raceList[currentRace].RaceRunnerCount}</h3>
-        <h3>
-          WEATHER - {raceList[currentRace].RaceWeatherCondition.toUpperCase()}
-        </h3>
-        <h3>
-          TRACK CONDITION -{' '}
-          {raceList[currentRace].RaceTrackCondition.toUpperCase()}
-        </h3>
-      </div>
-    );
+  } else console.log(`RaceName: `, raceList[currentRace].RaceName);
+  return (
+    <div className='show-raceinfo'>
+      <h2>{raceList[currentRace].RaceName}</h2>
+      <h3>RACE DISTANCE - {raceList[currentRace].RaceDistance}</h3>
+      <h3>RUNNERS - {raceList[currentRace].RaceRunnerCount}</h3>
+      <h3>
+        WEATHER - {raceList[currentRace].RaceWeatherCondition.toUpperCase()}
+      </h3>
+      <h3>
+        TRACK CONDITION -{' '}
+        {raceList[currentRace].RaceTrackCondition.toUpperCase()}
+      </h3>
+    </div>
+  );
 };
 export default RaceInfo;
