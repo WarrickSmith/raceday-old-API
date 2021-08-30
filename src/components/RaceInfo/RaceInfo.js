49ae7c9868814c69fc58a939a72d54d7ccd06907
Warrick <warricksmith@developersinstitute.co.nz>
2021-08-30 13:25:37 +1200

    fix: edit css

--- src/components/RaceInfo/RaceInfo.js
+++ src/components/RaceInfo/RaceInfo.js
@@ -1,4 +1,6 @@
 // Function to display Race Information for the currently selected race. A list of race meetings with races is parsed using currentRaceinfo to match meetingId and raceNumber. If a match is found, subsequent race informtion is then returned in the component.
+import './RaceInfo.css';
+
 const RaceInfo = ({ raceList, currentRace }) => {
   console.log(`RaceInfo Element Loading...`);
   console.log(`currentRace: `, currentRace);
@@ -11,16 +13,20 @@ const RaceInfo = ({ raceList, currentRace }) => {
   } else console.log(`RaceName: `, raceList[currentRace].RaceName);
   return (
     <div className='show-raceinfo'>
-      <h2>{raceList[currentRace].RaceName}</h2>
-      <h3>RACE DISTANCE - {raceList[currentRace].RaceDistance}</h3>
-      <h3>RUNNERS - {raceList[currentRace].RaceRunnerCount}</h3>
-      <h3>
-        WEATHER - {raceList[currentRace].RaceWeatherCondition.toUpperCase()}
-      </h3>
-      <h3>
-        TRACK CONDITION -{' '}
-        {raceList[currentRace].RaceTrackCondition.toUpperCase()}
-      </h3>
+      <div className='show-raceinfo-title'>
+        <h2>{raceList[currentRace].RaceName}</h2>
+      </div>
+      <div className='show-raceinfo-detail'>
+        <h3>RACE DISTANCE - {raceList[currentRace].RaceDistance}</h3>
+        <h3>RUNNERS - {raceList[currentRace].RaceRunnerCount}</h3>
+        <h3>
+          WEATHER - {raceList[currentRace].RaceWeatherCondition.toUpperCase()}
+        </h3>
+        <h3>
+          TRACK CONDITION -{' '}
+          {raceList[currentRace].RaceTrackCondition.toUpperCase()}
+        </h3>
+      </div>
     </div>
   );
 };
