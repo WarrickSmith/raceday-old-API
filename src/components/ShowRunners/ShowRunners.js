2524d2e19dbaa481f3e5f8e99efc3093a96faeb9
Warrick <warricksmith@developersinstitute.co.nz>
2021-09-01 13:12:32 +1200

    feat: add cee & new css variables for new elements

--- src/components/ShowRunners/ShowRunners.js
+++ src/components/ShowRunners/ShowRunners.js
@@ -1,4 +1,5 @@
 /* This function (Component) takes in an array of current race data with details of one specific race and the runners in that race. It then returns an element with race Runner information */
+import './ShowRunners.css';
 
 const ShowRunners = ({ currentRaceData }) => {
   console.log(`ShowRunners Element Loading...`);
@@ -10,17 +11,17 @@ const ShowRunners = ({ currentRaceData }) => {
   }
 
   return (
-    <div>
-      <div>
+    <div className='show-runners'>
+      <div className='runners'>
         <h2>Runner</h2>
         {currentRaceData && (
-          <ol id='runners' name='runners'>
+          <ol id='runner' name='runner'>
             {race.Runners.map((runner, index) => (
               <li key={index + runner.RunnerName}>
                 <div>
-                  {runner.RunnerNumber} {runner.RunnerName}
+                  {runner.RunnerNumber} &nbsp; {runner.RunnerName}
                 </div>
-                <div>
+                <div className='runner-margin'>
                   {runner.RiderName} ({runner.Barrier})
                 </div>
               </li>
@@ -28,19 +29,33 @@ const ShowRunners = ({ currentRaceData }) => {
           </ol>
         )}
       </div>
-      <div>
-        <br />
-        <h2>Win/Plc</h2>
-        {currentRaceData && (
-          <ol id='runners' name='runners'>
-            {race.Runners.map((runner, index) => (
-              <li key={index + runner.RunnerName}>
-                <div>{runner.WinOdss}</div>
-                <div>{runner.PlaceOdds}</div>
-              </li>
-            ))}
-          </ol>
-        )}
+      <div className='dividends'>
+        <div>
+          <h2>Win /</h2>
+          {currentRaceData && (
+            <ol id='win' name='win'>
+              {race.Runners.map((runner, index) => (
+                <li key={index + runner.RunnerName}>
+                  <div className='div-margin'>{runner.WinOdds}</div>
+                  <br />
+                </li>
+              ))}
+            </ol>
+          )}
+        </div>
+        <div>
+          <h2>Plc</h2>
+          {currentRaceData && (
+            <ol id='place' name='place'>
+              {race.Runners.map((runner, index) => (
+                <li key={index + runner.RunnerName}>
+                  <div className='div-margin'>{runner.PlaceOdds}</div>
+                  <br />
+                </li>
+              ))}
+            </ol>
+          )}
+        </div>
       </div>
     </div>
   );
